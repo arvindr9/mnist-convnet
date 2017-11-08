358fa1e4610a413e2cd374961a4c8ce9e05e6253
Arvind Ramaswami <saramaswami@gmail.com>
2017-11-08 12:27:10 -0300

    made some filereading code

---
+++ main.py
@@ -0,0 +1,9 @@
+#convnet in scrath from numpy(no tf for this file)
+import numpy as np
+
+with open("data/train-images-idx3-ubyte.gz") as img, open("data/train-labels-idx1-ubyte.gz") as lbl:
+    
+    X_train = np.fromfile(img, dtype=np.uint8)[16:]#.reshape(60000, 784)
+    print(X_train.shape)
+    Y_train = np.fromfile(lbl, dtype=np.uint8) 
+    print(Y_train.shape)
