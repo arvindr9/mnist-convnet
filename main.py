bb292b0a88c8edbbae6a586b00adadc73a841ae8
Ramaswami <aramaswami32@DesignCommons05.tss.gatech.edu>
2017-11-10 17:21:32 -0500

    unzipped the .gz files

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 
 with open("data/train-images-idx3-ubyte.gz") as img, open("data/train-labels-idx1-ubyte.gz") as lbl:
     
-    X_train = np.fromfile(img, dtype=np.uint8)[16:]#.reshape(60000, 784)
+    X_train = np.fromfile(img, dtype=np.uint8)#.reshape(60000, 784)
     print(X_train.shape)
+    print(X_train[: 30])
     Y_train = np.fromfile(lbl, dtype=np.uint8) 
     print(Y_train.shape)
