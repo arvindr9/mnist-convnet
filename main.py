eda0c9b507efe228517b10f98655a3a27f20ee3b
Ramaswami <aramaswami32@DesignCommons05.tss.gatech.edu>
2017-11-10 17:31:28 -0500

    successfully added file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #convnet in scrath from numpy(no tf for this file)
 import numpy as np
 
-with open("data/train-images-idx3-ubyte.gz") as img, open("data/train-labels-idx1-ubyte.gz") as lbl:
+with open("data/train-images-idx3-ubyte") as img, open("data/train-labels-idx1-ubyte") as lbl:
     
-    X_train = np.fromfile(img, dtype=np.uint8)#.reshape(60000, 784)
+    X_train = np.fromfile(img, dtype=np.uint8)[16:].reshape(784, 60000)
     print(X_train.shape)
-    print(X_train[: 30])
-    Y_train = np.fromfile(lbl, dtype=np.uint8) 
+    print(X_train[0][500])
+    Y_train = np.fromfile(lbl, dtype=np.uint8)[8:].reshape(1, 60000)
     print(Y_train.shape)
